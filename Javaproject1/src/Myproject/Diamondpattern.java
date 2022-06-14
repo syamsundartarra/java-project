9a2db5ce419cfd6a05fd4773a922d944247c82a0
syamsundar tarra <syamsundar tarra@LAPTOP-S2465QQ6>
2022-06-14 12:48:01 +0530

    first commit

---
+++ Javaproject1/src/Myproject/Diamondpattern.java
@@ -0,0 +1,45 @@
+package Myproject;
+public class Diamondpatten{
+public static void main(String[] args) 
+{ 
+int size=5;
+for(int i=1;i<=size;i++)
+{
+for(int j=size;j>i;j--)
+{
+System.out.print(" ");
+}
+for(int k=0;k<i*2-1;k++)
+{
+if(k==0||k==2*i-2)
+{
+System.out.print("*");
+}
+else
+{
+System.out.print(" ");
+}
+}
+System.out.println();
+}
+for(int i=1;i<size;i++)
+{
+for(int j=0;j<i;j++)
+{
+System.out.print(" ");
+}
+for(int k=(size-i)*2-1;k>=1;k--)
+{
+if(k==1||k==(size-i)*2-1)
+{
+System.out.print("*");
+}
+else
+{
+System.out.print(" ");
+}
+}
+System.out.println();
+}
+}
+}
